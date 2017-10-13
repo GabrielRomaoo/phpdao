3c6f8b6e6928a0aef8a62a1c863627ce4fedcf6a
DESKTOP-7RO50IG\MrRomaoo <gabriel.romao@outlook.com.br>
2017-10-13 18:45:24 -0300

    criamso uma lista em usuario.php

--- class/Usuario.php
+++ class/Usuario.php
@@ -70,6 +70,55 @@
 
 	}
 
+
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+	}
+
+	public static function searchByLogin($login){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ", array(':SEARCH'=>"%".$login."%"));
+
+
+	}
+
+	public function validaLogin($login , $password){
+
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login , 
+			":PASSWORD"=>$password
+			));
+
+		if(count($results) > 0 ){
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		
+
+		}
+
+		else{
+
+			throw new Exception("Login e/ou Senha inválidos.");
+
+		}
+
+	}
+
+
 	public function __toString(){
 
 		return json_encode(array(
--- index.php
+++ index.php
@@ -4,18 +4,42 @@
 	require_once("config.php");
 
 
-	/*$sql = new Sql();
+	//carrega apenas um usuário do banco, pelo seu ID correspondente
 
-
-	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-	echo json_encode($usuarios);
-	*/
-
+	/*
 	$root = new Usuario();
 
 	$root->loadbyId(1);
 
 	echo $root;
+	*/
+// ----------------------------------------------
+
+	// irá carregar uma lista de usuários
+
+	//$lista = Usuario::getList();
+
+	//echo json_encode(($lista));
+
+//------------------------------------------------------------
+
+	// executa uma busca no banco por login do usuário
+
+	//$search = Usuario::searchByLogin("ro");
+
+	//	echo json_encode($search);
+ 
+// ---------------------------------------------------------
+
+	// carrega um usuário autenticando o login e a senha
+
+
+	$usuario = new Usuario();	
+
+	$usuario->validaLogin("Romão", "requeijao");
+
+
+	echo $usuario;
+
 
  ?>
